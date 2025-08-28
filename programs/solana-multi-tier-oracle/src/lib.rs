a3c7d6db1d8c6cef797d792cafafa0b4f829329c
Srivatsav Erramilli <srivatsaverramilli@gmail.com>
2025-08-28 15:44:19 +0530

    anchor project initialization

---
+++ programs/solana-multi-tier-oracle/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("4CLGL8iE73T7Wcwjt3q2XapX22iSxPpXFpYGhZ33yc9h");
+
+#[program]
+pub mod solana_multi_tier_oracle {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
